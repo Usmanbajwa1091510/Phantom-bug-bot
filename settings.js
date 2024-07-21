72709b8ecdf88f5ed6a8c035c2ac9ee140e879c6
Usmanbajwa1091510 <tytrrrrtrrr16@gmail.com>
2024-07-21 06:12:36 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,14 +8,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348108395270"
-global.ownername = "*Phantom+Hacker"
+global.ownernomer = "923105171009"
+global.ownername = "*Usman+Bhatti"
 global.ytname = "YT: phantom-hacks01"
 global.socialm = ""
 global.location = "India, Mizoram, Aizawl"
 
-global.ownernumber = '2348108395270'  //creator number
-global.ownername = 'Phantom+Hacker' //owner name
+global.ownernumber = '923105171009'  //creator number
+global.ownername = 'Usman+Bhatti' //owner name
 global.botname = 'xbug bot' //name of the bot
 
 //sticker details
@@ -33,13 +33,13 @@ global.link = 'https://whatsapp.com/channel/0029VaBROanEquiLW3ii1n47'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
+global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = false //auto view status/story
+global.autoswview = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
